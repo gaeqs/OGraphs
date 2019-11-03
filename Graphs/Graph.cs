9424724a0eabd6aed6bf2692b6e3ac0aac4d1201
gaeqs <gael.rial.costas@gmail.com>
2019-11-04 00:51:02 +0100

    Initial commit

---
+++ Graphs/Graph.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Text;
+
+namespace Graphs{
+    /**
+     * Represents a column-stored graph.
+     */
+    public class Graph{
+        private readonly float[,] matrix_;
+        private readonly int length_;
+
+        public Graph(float[,] matrix) {
+            if (matrix.GetLength(0) != matrix.GetLength(1))
+                throw new ArgumentException("Matrix must be a square matrix.");
+            matrix_ = matrix;
+            length_ = matrix.GetLength(0);
+
+            //Let's make sure diagonals are 0
+            for (var i = 0; i < length_; i++) {
+                matrix_[i, i] = 0;
+            }
+
+            //Let's make sure it's not directed
+            for (var x = 0; x < length_; x++) {
+                for (var y = x + 1; y < length_; y++) {
+                    matrix_[y, x] = matrix_[x, y];
+                }
+            }
+        }
+
+        public Graph(int length) {
+            if (length <= 0)
+                throw new ArgumentException("Length must be greater than 0.");
+            length_ = length;
+            matrix_ = new float[length, length];
+        }
+
+        public Graph(Graph graph) {
+            matrix_ = graph.matrix_.Clone() as float[,];
+            if (matrix_ == null) {
+                throw new Exception("Matrix clone failed.");
+            }
+
+            length_ = matrix_.GetLength(0);
+        }
+
+        public float[,] Matrix => matrix_;
+
+        public float GetValue(int x, int y) {
+            return matrix_[x, y];
+        }
+
+        public void SetValue(int x, int y, float value) {
+            matrix_[x, y] = matrix_[y, x] = value;
+        }
+
+        public Graph GetMinimumCostGraph() {
+            var costGraph = new Graph(this);
+
+            for (var k = 0; k < length_; k++) {
+                for (var i = 0; i < length_; i++) {
+                    for (var j = 0; j < length_; j++) {
+                        var dt = costGraph[i, k] + costGraph[k, j];
+                        if (costGraph[i, j] < dt) continue;
+                        costGraph[i, j] = dt;
+                    }
+                }
+            }
+
+            return costGraph;
+        }
+
+        public override string ToString() {
+            var builder = new StringBuilder();
+            for (var y = 0; y < length_; y++) {
+                for (var x = 0; x < length_; x++) {
+                    builder.Append(Math.Round(matrix_[x, y], 2));
+                    if (x == length_ - 1) continue;
+                    builder.Append("\t");
+                }
+
+                builder.Append('\n');
+            }
+
+            return builder.ToString();
+        }
+
+        public float this[int x, int y] {
+            get => GetValue(x, y);
+            set => SetValue(x, y, value);
+        }
+    }
+}
---
+++ Graphs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Graphs{
+    class Program{
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+
+            const int length = 10;
+
+            var random = new Random();
+            var matrix = new float[length, length];
+            for (var x = 0; x < length; x++) {
+                for (var y = x + 1; y < length; y++) {
+                    matrix[x, y] = (float) (random.NextDouble() < 0.8 ? random.NextDouble() * 100 : double.PositiveInfinity);
+                }
+            }
+            
+            var graph = new Graph(matrix);
+            Console.WriteLine(graph);
+            Console.WriteLine("---------------------------");
+            Console.WriteLine(graph.GetMinimumCostGraph());
+        }
+    }
+}
