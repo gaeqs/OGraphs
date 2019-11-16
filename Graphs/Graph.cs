73190d5379f6494bae61d8e2dfc2a53edefcb361
gaeqs <gael.rial.costas@gmail.com>
2019-11-16 01:29:18 +0100

    Added load and save methods.

--- Graphs/Graph.cs
+++ Graphs/Graph.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Text;
 
 namespace Graphs{
@@ -6,24 +7,24 @@ namespace Graphs{
      * Represents a column-stored graph.
      */
     public class Graph{
-        private readonly float[,] matrix_;
-        private readonly int length_;
+        private readonly float[,] _matrix;
+        private readonly int _length;
 
         public Graph(float[,] matrix) {
             if (matrix.GetLength(0) != matrix.GetLength(1))
                 throw new ArgumentException("Matrix must be a square matrix.");
-            matrix_ = matrix;
-            length_ = matrix.GetLength(0);
+            _matrix = matrix;
+            _length = matrix.GetLength(0);
 
             //Let's make sure diagonals are 0
-            for (var i = 0; i < length_; i++) {
-                matrix_[i, i] = 0;
+            for (var i = 0; i < _length; i++) {
+                _matrix[i, i] = 0;
             }
 
             //Let's make sure it's not directed
-            for (var x = 0; x < length_; x++) {
-                for (var y = x + 1; y < length_; y++) {
-                    matrix_[y, x] = matrix_[x, y];
+            for (var x = 0; x < _length; x++) {
+                for (var y = x + 1; y < _length; y++) {
+                    _matrix[y, x] = _matrix[x, y];
                 }
             }
         }
@@ -31,46 +32,64 @@ namespace Graphs{
         public Graph(int length) {
             if (length <= 0)
                 throw new ArgumentException("Length must be greater than 0.");
-            length_ = length;
-            matrix_ = new float[length, length];
+            _length = length;
+            _matrix = new float[length, length];
         }
 
         public Graph(Graph graph) {
-            matrix_ = graph.matrix_.Clone() as float[,];
-            if (matrix_ == null) {
+            _matrix = graph._matrix.Clone() as float[,];
+            if (_matrix == null) {
                 throw new Exception("Matrix clone failed.");
             }
 
-            length_ = matrix_.GetLength(0);
+            _length = _matrix.GetLength(0);
         }
 
-        public float[,] Matrix => matrix_;
+        public Graph(string path) {
+            var stream = new BinaryReader(File.OpenRead(path), Encoding.ASCII);
+            _length = stream.ReadInt32();
+            if (_length <= 0) {
+                stream.Close();
+                throw new ArgumentException("Length must be greater than 0.");
+            }
+
+            _matrix = new float[_length, _length];
+
+            for (var row = 0; row < _length; row++) {
+                for (var column = 0; column < _length; column++) {
+                    _matrix[row, column] = stream.ReadSingle();
+                }
+            }
+
+            stream.Close();
+        }
+
+        public float[,] Matrix => _matrix;
 
         public float GetValue(int x, int y) {
-            return matrix_[x, y];
+            return _matrix[x, y];
         }
 
         public void SetValue(int x, int y, float value) {
-            matrix_[x, y] = matrix_[y, x] = value;
+            _matrix[x, y] = _matrix[y, x] = value;
         }
 
         public void GetMinimumCostGraph(out Graph costGraph, out Graph predecessorsGraph) {
             costGraph = new Graph(this);
 
-           predecessorsGraph = new Graph(length_);
-            for (var i = 0; i < length_; i++) {
-                for (var j = 0; j < length_; j++) {
-                    predecessorsGraph[i, j] = i == j || float.IsPositiveInfinity(matrix_[i, j]) ? float.NaN : i;
+            predecessorsGraph = new Graph(_length);
+            for (var i = 0; i < _length; i++) {
+                for (var j = 0; j < _length; j++) {
+                    predecessorsGraph[i, j] = i == j || float.IsPositiveInfinity(_matrix[i, j]) ? float.NaN : i;
                 }
             }
-            
-            
 
-            for (var k = 0; k < length_; k++) {
-                for (var i = 0; i < length_; i++) {
-                    for (var j = 0; j < length_; j++) {
+
+            for (var k = 0; k < _length; k++) {
+                for (var i = 0; i < _length; i++) {
+                    for (var j = 0; j < _length; j++) {
                         var dt = costGraph[i, k] + costGraph[k, j];
-                        if (costGraph[i, j] < dt) continue;
+                        if (costGraph[i, j] <= dt) continue;
                         costGraph[i, j] = dt;
                     }
                 }
@@ -79,10 +98,10 @@ namespace Graphs{
 
         public override string ToString() {
             var builder = new StringBuilder();
-            for (var y = 0; y < length_; y++) {
-                for (var x = 0; x < length_; x++) {
-                    builder.Append(Math.Round(matrix_[x, y], 2));
-                    if (x == length_ - 1) continue;
+            for (var y = 0; y < _length; y++) {
+                for (var x = 0; x < _length; x++) {
+                    builder.Append(Math.Round(_matrix[x, y], 2));
+                    if (x == _length - 1) continue;
                     builder.Append("\t");
                 }
 
@@ -95,6 +114,19 @@ namespace Graphs{
         public float this[int x, int y] {
             get => GetValue(x, y);
             set => SetValue(x, y, value);
+        }
+
+        public void Write(string path) {
+            var stream = new BinaryWriter(File.OpenWrite(path), Encoding.ASCII);
+            stream.Write(_length);
+
+            for (var row = 0; row < _length; row++) {
+                for (var column = 0; column < _length; column++) {
+                    stream.Write(_matrix[row, column]);
+                }
+            }
+
+            stream.Close();
         }
     }
 }
--- Graphs/Program.cs
+++ Graphs/Program.cs
@@ -7,23 +7,38 @@ namespace Graphs{
 
             const int length = 10;
 
-            var random = new Random();
-            var matrix = new float[length, length];
-            for (var x = 0; x < length; x++) {
-                for (var y = x + 1; y < length; y++) {
-                    matrix[x, y] = (float) (random.NextDouble() < 0.8
-                        ? random.NextDouble() * 100
-                        : double.PositiveInfinity);
-                }
-            }
+            //var random = new Random();
+            //var matrix = new float[length, length];
+            //for (var x = 0; x < length; x++) {
+            //    for (var y = x + 1; y < length; y++) {
+            //        matrix[x, y] = (float) (random.NextDouble() < 0.8
+            //            ? random.NextDouble() * 100
+            //            : double.PositiveInfinity);
+            //    }
+            //}
 
-            var graph = new Graph(matrix);
+            var graph = new Graph(@"D:\graph.dat");
             Console.WriteLine(graph);
+            
+            var savedPredecessors = new Graph(@"D:\predecessors.dat");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine(savedPredecessors);
+            
+            var savedCosts = new Graph(@"D:\cost.dat");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine(savedCosts);
+            
+            
+            
+            
+            Console.WriteLine("---------------------------");
             Console.WriteLine("---------------------------");
             graph.GetMinimumCostGraph(out var cost, out var predecessors);
-            Console.WriteLine(cost);
-            Console.WriteLine("---------------------------");
             Console.WriteLine(predecessors);
+            Console.WriteLine("---------------------------");
+            Console.WriteLine(cost);
+            
+            graph.Write(@"D:\graph.dat");
         }
     }
 }
